ad15e27ca7ce4ef8e7b6894b8f1d36cf026f8063
Abrahan987 <168798723+Abrahan987@users.noreply.github.com>
2026-09-22 18:57:29 +0000

    Update config.ts

--- config.ts
+++ config.ts
@@ -1,9 +1,8 @@
 import { watchFile, unwatchFile } from 'fs';
 import { fileURLToPath } from 'url';
-
-global.emojis = '';
+global.owner = '573237649689';
+global.nmcreador '𝞯𝞯🪷 𝐀𝐁𝐑𝐀𝐇𝐀𝐍-𝐌 ˙';
 global.namebot = '𖫨𖫨🪷⃨᪲  𝐘𝐀𝐄 𝐌𝐈𝐊𝐔 𝗕𝗢𝗧˙ᰨᰍ';
-global.rcanal = '';
 global.prefix = ['.', '#']
 global.banner = 'https://raw.githubusercontent.com/IrokzDal/data/main/1789777142507.jpeg';
 global.icono = 'https://raw.githubusercontent.com/IrokzDal/data/main/1789776770221.jpeg';
