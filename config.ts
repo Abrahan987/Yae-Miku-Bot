01446a5c292b7a0995e0df78163b2f3322803316
Abrahan987 <168798723+Abrahan987@users.noreply.github.com>
2026-09-23 14:36:05 +0000

    Update config.ts

--- config.ts
+++ config.ts
@@ -1,7 +1,7 @@
 import { watchFile, unwatchFile } from 'fs';
 import { fileURLToPath } from 'url';
 global.owner = '573237649689';
-global.nmcreador '𝞯𝞯🪷 𝐀𝐁𝐑𝐀𝐇𝐀𝐍-𝐌 ˙';
+global.nmcreador = '𝞯𝞯🪷 𝐀𝐁𝐑𝐀𝐇𝐀𝐍-𝐌 ˙';
 global.namebot = '𖫨𖫨🪷⃨᪲  𝐘𝐀𝐄 𝐌𝐈𝐊𝐔 𝗕𝗢𝗧˙ᰨᰍ';
 global.prefix = ['.', '#']
 global.banner = 'https://raw.githubusercontent.com/IrokzDal/data/main/1789777142507.jpeg';
