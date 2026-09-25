e8ca0ee668ebfda470b97cc5244fc460bdd956ac
Abrahan987 <168798723+Abrahan987@users.noreply.github.com>
2026-09-25 16:30:26 +0000

    Update tiktok.ts

--- src/plugins/tiktok.ts
+++ src/plugins/tiktok.ts
@@ -9,9 +9,12 @@ export default async function (sock: any, msg: any, extra: any, db: any) {
 
     if (!url) {
         return msg.reply(
-            `🍓 𝚃𝙸𝙺𝚃𝙾𝙺\n\n` +
-            `𝙴𝙽𝚅Í𝙰 𝙴𝙻 𝙴𝙽𝙻𝙰𝙲𝙴 𝙳𝙴𝙻 𝚅Í𝙳𝙴𝙾\n\n` +
-            `🪷 𝙴𝙹𝙴𝙼𝙿𝙻𝙾\n` +
+            `ᅟㅤ 𓈒    |꛱ ᷼ |꛱ ᷼ |ㅤֵㅤ  ̄ 𐇽 🍓 ㅤ࣫ㅤ|꛱ ᷼ |꛱ ᷼ |ㅤ 𓈒\n\n` +
+            `𖫨𖫨🪷⃨᪲  𖫨𖫨🪷⃨᪲  ${global.namebot}\n` +
+            `𐴲੭  ˙ 𓂃  🍥  𓂃  ˙\n\n` +
+            `🍓͜ᩧ𑂳ᰍ  𝚃𝙸𝙺𝚃𝙾𝙺\n\n` +
+            `🪷 𝙴𝙽𝚅Í𝙰 𝙴𝙻 𝙴𝙽𝙻𝙰𝙲𝙴 𝙳𝙴𝙻 𝚅Í𝙳𝙴𝙾\n\n` +
+            `🍥 𝙴𝙹𝙴𝙼𝙿𝙻𝙾\n` +
             `> ${global.prefix[0]}𝚝𝚒𝚔𝚝𝚘𝚔 https://vt.tiktok.com/...`
         );
     }
@@ -24,7 +27,10 @@ export default async function (sock: any, msg: any, extra: any, db: any) {
 
     try {
         await msg.reply(
-            `🍓 𝚃𝙸𝙺𝚃𝙾𝙺\n\n` +
+            `ᅟㅤ 𓈒    |꛱ ᷼ |꛱ ᷼ |ㅤֵㅤ  ̄ 𐇽 🍓 ㅤ࣫ㅤ|꛱ ᷼ |꛱ ᷼ |ㅤ 𓈒\n\n` +
+            `𖫨𖫨🪷⃨᪲  𖫨𖫨🪷⃨᪲  ${global.namebot}\n` +
+            `𐴲੭  ˙ 𓂃  🍥  𓂃  ˙\n\n` +
+            `🍓͜ᩧ𑂳ᰍ  𝚃𝙸𝙺𝚃𝙾𝙺\n\n` +
             `🪷 𝙱𝚄𝚂𝙲𝙰𝙽𝙳𝙾 𝙸𝙽𝙵𝙾𝚁𝙼𝙰𝙲𝙸Ó𝙽...`
         );
 
@@ -54,43 +60,15 @@ export default async function (sock: any, msg: any, extra: any, db: any) {
 
         const title = info.title || '𝚂𝙸𝙽 𝚃Í𝚃𝚄𝙻𝙾';
         const duration = info.duration || '𝙳𝙴𝚂𝙲𝙾𝙽𝙾𝙲𝙸𝙳𝙰';
-        const region = info.region || '𝙳𝙴𝚂𝙲𝙾𝙽𝙾𝙲𝙸𝙳𝙰';
-        const createdAt = info.created_at || '𝙳𝙴𝚂𝙲𝙾𝙽𝙾𝙲𝙸𝙳𝙰';
-
-        const nickname =
-            info.author?.nickname || '𝙳𝙴𝚂𝙲𝙾𝙽𝙾𝙲𝙸𝙳𝙾';
-
-        const uniqueId =
-            info.author?.unique_id || '𝙳𝙴𝚂𝙲𝙾𝙽𝙾𝙲𝙸𝙳𝙾';
+        const nickname = info.author?.nickname || '𝙳𝙴𝚂𝙲𝙾𝙽𝙾𝙲𝙸𝙳𝙾';
 
         const plays = Number(info.stats?.plays || 0);
         const likes = Number(info.stats?.likes || 0);
         const comments = Number(info.stats?.comments || 0);
         const shares = Number(info.stats?.shares || 0);
-        const downloads = Number(info.stats?.downloads || 0);
 
         const formatNumber = (number: number) =>
             new Intl.NumberFormat('es-CO').format(number);
-
-        await msg.reply(
-            `🍓 𝚅Í𝙳𝙴𝙾\n\n` +
-
-            `> 𝚃Í𝚃𝚄𝙻𝙾 ── ${title}\n` +
-            `> 𝙰𝚄𝚃𝙾𝚁 ── ${nickname}\n` +
-            `> 𝚄𝚂𝙴𝚁 ── @${uniqueId}\n` +
-            `> 𝙳𝚄𝚁𝙰𝙲𝙸Ó𝙽 ── ${duration}\n` +
-            `> 𝚁𝙴𝙶𝙸Ó𝙽 ── ${region}\n` +
-            `> 𝙵𝙴𝙲𝙷𝙰 ── ${createdAt}\n\n` +
-
-            `🪷 𝙴𝚂𝚃𝙰𝙳Í𝚂𝚃𝙸𝙲𝙰𝚂\n\n` +
-            `> 𝚅𝙸𝚂𝚃𝙰𝚂 ── ${formatNumber(plays)}\n` +
-            `> 𝙻𝙸𝙺𝙴𝚂 ── ${formatNumber(likes)}\n` +
-            `> 𝙲𝙾𝙼𝙴𝙽𝚃𝙰𝚁𝙸𝙾𝚂 ── ${formatNumber(comments)}\n` +
-            `> 𝙲𝙾𝙼𝙿𝙰𝚁𝚃𝙸𝙳𝙾𝚂 ── ${formatNumber(shares)}\n` +
-            `> 𝙳𝙴𝚂𝙲𝙰𝚁𝙶𝙰𝚂 ── ${formatNumber(downloads)}\n\n` +
-
-            `🍥 𝙿𝚁𝙴𝙿𝙰𝚁𝙰𝙽𝙳𝙾 𝚅Í𝙳𝙴𝙾...`
-        );
 
         const videoResponse = await axios.get(info.dl, {
             responseType: 'arraybuffer',
@@ -100,7 +78,7 @@ export default async function (sock: any, msg: any, extra: any, db: any) {
             maxRedirects: 10,
             headers: {
                 'User-Agent': 'Mozilla/5.0',
-                'Accept': 'video/mp4,video/*,*/*'
+                Accept: 'video/mp4,video/*,*/*'
             }
         });
 
@@ -116,20 +94,15 @@ export default async function (sock: any, msg: any, extra: any, db: any) {
                 video: videoBuffer,
                 mimetype: 'video/mp4',
                 caption:
-                    `🍓 𝚅Í𝙳𝙴𝙾 𝙳𝙴𝚂𝙲𝙰𝚁𝙶𝙰𝙳𝙾\n\n` +
-
+                    `🍓͜ᩧ𑂳ᰍ  𝚅Í𝙳𝙴𝙾 𝙳𝙴𝚂𝙲𝙰𝚁𝙶𝙰𝙳𝙾\n\n\n` +
                     `🪷 𝚃Í𝚃𝚄𝙻𝙾 ── ${title}\n` +
-                    `> 𝙰𝚄𝚃𝙾𝚁 ── ${nickname}\n` +
-                    `> 𝚄𝚂𝙴𝚁 ── @${uniqueId}\n` +
-                    `> 𝙳𝚄𝚁𝙰𝙲𝙸Ó𝙽 ── ${duration}\n\n` +
-
+                    `🍥 𝙰𝚄𝚃𝙾𝚁 ── ${nickname}\n` +
+                    `> 𝙳𝚄𝚁𝙰𝙲𝙸Ó𝙽 ── ${duration}\n` +
                     `> 𝚅𝙸𝚂𝚃𝙰𝚂 ── ${formatNumber(plays)}\n` +
                     `> 𝙻𝙸𝙺𝙴𝚂 ── ${formatNumber(likes)}\n` +
                     `> 𝙲𝙾𝙼𝙴𝙽𝚃𝙰𝚁𝙸𝙾𝚂 ── ${formatNumber(comments)}\n` +
-                    `> 𝙲𝙾𝙼𝙿𝙰𝚁𝚃𝙸𝙳𝙾𝚂 ── ${formatNumber(shares)}\n` +
-                    `> 𝙳𝙴𝚂𝙲𝙰𝚁𝙶𝙰𝚂 ── ${formatNumber(downloads)}\n\n` +
-
-                    `🍥 ${global.nmcreador}`
+                    `> 𝙲𝙾𝙼𝙿𝙰𝚁𝚃𝙸𝙳𝙾𝚂 ── ${formatNumber(shares)}\n\n` +
+                    `ꨄ︎ ${global.nmcreador}`
             },
             {
                 quoted: msg
