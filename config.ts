f712f795eef3359e626d6e1891239416a60035f9
Abrahan987 <168798723+Abrahan987@users.noreply.github.com>
2026-09-26 12:39:00 +0000

    Update config.ts

--- config.ts
+++ config.ts
@@ -7,10 +7,8 @@ global.prefix = ['.', '#']
 global.banner = 'https://raw.githubusercontent.com/IrokzDal/data/main/1789777142507.jpeg';
 global.icono = 'https://raw.githubusercontent.com/IrokzDal/data/main/1789776770221.jpeg';
 global.api = '';
-global.key = 'proyectsV2'
-global.my = {
-    ch: '',
-    ch2: ''
+global.key = 'proyectsV2';
+global.rcanal = '120363427270057983@newsletter';
 };
 
 const file = fileURLToPath(import.meta.url);
