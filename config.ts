21f8a0606f403a44172d18ffe72061a6bf8452ab
Abrahan987 <168798723+Abrahan987@users.noreply.github.com>
2026-09-26 12:47:43 +0000

    Update config.ts

--- config.ts
+++ config.ts
@@ -1,15 +1,16 @@
 import { watchFile, unwatchFile } from 'fs';
 import { fileURLToPath } from 'url';
+
 global.owner = '573237649689';
 global.nmcreador = '𝞯𝞯🪷 𝐀𝐁𝐑𝐀𝐇𝐀𝐍-𝐌 ˙';
 global.namebot = '𖫨𖫨🪷⃨᪲  𝐘𝐀𝐄 𝐌𝐈𝐊𝐔 𝗕𝗢𝗧˙ᰨᰍ';
-global.prefix = ['.', '#']
+global.prefix = ['.', '#'];
 global.banner = 'https://raw.githubusercontent.com/IrokzDal/data/main/1789777142507.jpeg';
 global.icono = 'https://raw.githubusercontent.com/IrokzDal/data/main/1789776770221.jpeg';
 global.api = '';
 global.key = 'proyectsV2';
-global.rcanal = '120363427270057983@newsletter'
-};
+global.rcanal = '120363427270057983@newsletter';
+
 
 const file = fileURLToPath(import.meta.url);
 
